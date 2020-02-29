bab3238f7569aebe1551d5fc7954f0b2928688da
geraldgsh <geraldgsh@hotmail.com>
2020-02-29 08:01:34 +0800

    completed mockup

--- script.js
+++ script.js
@@ -27,34 +27,48 @@ const showDate = (() => {
 })();
 
 const renderTaskCard = (name, projectID, title, desc, dueDate, priority, done, id) => {
+  const later = new Date(dueDate.split("-").join(","))
+  const today  = new Date();
+  const remainingDays = Math.ceil((later - today) / (1000 * 3600 * 24))
+  
   const CHECK = 'fa-check-circle';
   const UNCHECK = 'fa-circle';
   const LINE_THROUGH = "lineThrough";
   const DONE = done ? CHECK : UNCHECK;
   const LINE = done ? LINE_THROUGH : "";
   const item = `<div class="card">
-                <header class="card-header">
-                  <div>
-                    <ul>
-                      <li class="item">                        
-                        <p class="text ${LINE} ${name}${id}">Project: ${name}</p>
-                        <i class="far ${DONE} co" job="complete" id="${name}${id}" pid="${projectID}" tid="${id}"></i>
-                        <i class="fas fa-trash-alt de" job="delete" pid="${projectID}" tid="${id}"></i>
-                        <i class="fas fa-edit se" job="edit" pid="${projectID}" tid="${id}" id="editTask"></i>
-                      </li>
-                    </ul>
+                  <header class="card-header">
+                    <div>
+                      <ul>
+                        <li class="item">                        
+                          <p class="text ${LINE} ${name}${id}">Project: ${name}</p>
+                          <i class="far ${DONE} co" job="complete" id="${name}${id}" pid="${projectID}" tid="${id}"></i>
+                          <i class="fas fa-trash-alt de" job="delete" pid="${projectID}" tid="${id}"></i>
+                          <i class="fas fa-edit se" job="edit" id="${name}${id}" pid="${projectID}" tid="${id}"></i>
+                        </li>
+                      </ul>
+                    </div>
+                  </header>
+                  <div class="card-content">
+                    <div class="content">
+                      <h4 class="text is-4 ${LINE} ${name}${id}">Task: ${title}</h4>
+                      <p class="text ${LINE} ${name}${id}">Description: ${desc}</p>
+                      <p class="text ${LINE} ${name}${id}"><time datetime="2016-1-1">Due Date: ${dueDate}</time></p>
+                      <p class="text ${LINE} ${name}${id}">Day(s) left: ${remainingDays}</p>
+                      <p class="text ${LINE} ${name}${id}">Priority: ${priority}</p>
+                      <input id="titleEdit${projectID}${id}" class='text ${name}${id}' placeholder='Task: ${title}'></input>
+                      <textarea id="descEdit${projectID}${id}" class="textarea is-info ${name}${id}" maxlength="128" placeholder='Description: ${desc}'></textarea>
+                      <input id="dateEdit${projectID}${id}" class='text ${name}${id}' id="dateEdit" type="date" placeholder='${dueDate}'>                      
+                      <div class="selection ${name}${id}">
+                        <select id="priorityEdit${projectID}${id}">
+                          <option value="Low">Low</option>
+                          <option value="High">High</option>
+                        </select>
+                      </div>
+                      <button id='editTask' class="${name}${id}" job="save" pid="${projectID}" tid="${id}">Save changes</button>
+                    </div>                    
                   </div>
-                </header>
-                <div class="card-content">
-                  <div class="content">
-                    <h4 class="text is-4 ${LINE} ${name}${id}" id='st'>Task: ${title}</h4>
-                    <p class="text ${LINE} ${name}${id}">Description: ${desc}</p>
-                    <p class="text ${LINE} ${name}${id}"><time datetime="2016-1-1">Due Date: ${dueDate}</time></p>
-                    <p class="text ${LINE} ${name}${id}">Priority: ${priority}</p>
-                  </div>
-                </div>
-              </div>`
-
+                </div>`
   const position = 'afterbegin'
   list.insertAdjacentHTML(position, item);
 };
@@ -219,9 +233,12 @@ const completeTask = (element) => {
   element.classList.toggle(UNCHECK);
   projectList[pid].tasks[tid].done = projectList[pid].tasks[tid].done ? false : true;
   updateLocalStorage(projectList);
-  const target = document.getElementsByClassName(element.id);
+  const target = document.getElementsByClassName(element.id);  
   for (var i in target) {
-    target[i].classList.toggle(LINE_THROUGH);
+    if (target[i].classList == undefined) {
+    } else {
+      target[i].classList.toggle(LINE_THROUGH);
+    }    
   }  
 }
 
@@ -237,21 +254,30 @@ const removeTask = (element) => {
 const updateTask = (element) => {
   const pid = element.attributes.pid.value;
   const tid = element.attributes.tid.value;
-  const taskArr = projectList[pid].tasks[tid];
-  console.warn(taskArr);
-  projectList[pid].tasks[tid].title = document.getElementById('titleEdit').value;
-  projectList[pid].tasks[tid].dueDate = document.getElementById('dateEdit').value;
-  projectList[pid].tasks[tid].desc = document.getElementById('descEdit').value;
-  projectList[pid].tasks[tid].priority = document.getElementById('priorityEdit').value;
-  console.warn(taskArr);
+  const titleEdit = document.getElementById('titleEdit'+pid+tid).value;
+  const dateEdit = document.getElementById('dateEdit'+pid+tid).value;
+  const descEdit = document.getElementById('descEdit'+pid+tid).value;
+  if (titleEdit === '' || dateEdit === '' || descEdit === '') {
+    alert('Please fill in missing field(s) and/or later date');
+  } else {
+    projectList[pid].tasks[tid].title = titleEdit;
+    projectList[pid].tasks[tid].dueDate = dateEdit;
+    projectList[pid].tasks[tid].desc = descEdit;
+    projectList[pid].tasks[tid].priority = document.getElementById('priorityEdit'+pid+tid).value;
+    updateLocalStorage(projectList);
+    window.location.reload();
+  }
 }
 
-const editTask = (element) => {
-  const taskEditModal = new BulmaModal("#taskEditModal");  
-  taskEditModal.show();
-  document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('editTask').addEventListener('click', updateTask(element));
-  });
+const editTask = function(element) {
+  const SHOW = "show";
+  const target = document.getElementsByClassName(element.id);  
+  for (let i in target) {
+    if (target[i].classList == undefined) {
+    } else {   
+    target[i].classList.toggle(SHOW);
+    }
+  }  
 }
 
 const removeProject = (() => {
@@ -269,16 +295,11 @@ const modalOpen = (() => {
   const projectModal = new BulmaModal("#projectModal");
   const newTask = document.querySelector("#newTask");
   const taskModal = new BulmaModal("#taskModal");
-  const closeEditTask = document.querySelector("#closeEditTask");
-  const taskEditModal = new BulmaModal("#taskEditModal"); 
   newProject.addEventListener("click", function () {
     projectModal.show()  
   });  
   newTask.addEventListener("click", function () {
     taskModal.show()
-  });
-  closeEditTask.addEventListener("click", function () {
-    taskEditModal.show()
   });
 })()
 
@@ -286,15 +307,21 @@ const taskUpdate = (() => {
   const list = document.getElementById("list");
   // target the items create dynamically
   list.addEventListener("click", function(event) {
+    event.preventDefault();
     const element =  event.target; //return the clicked element inside list
-    const elementJob = element.attributes.job.value; // complete or delete 
-    if (elementJob == "complete") {
-      completeTask(element);
-    } else if (elementJob == "delete") {
-      removeTask(element);
-    } else if (elementJob == "edit") {
-      editTask(element);
-    }
+    if (element.attributes.job == undefined) {
+    } else {      
+      const elementJob = element.attributes.job.value; // complete or delete 
+      if (elementJob == "complete") {      
+        completeTask(element);      
+      } else if (elementJob == "delete") {
+        removeTask(element);
+      } else if (elementJob == "edit") {
+        editTask(element);
+      } else if (elementJob == "save") {
+        updateTask(element); 
+      }
+    }    
   });
 })();
 
